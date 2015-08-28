8f77dc9db3f40e9f22026a5a4f84289c4c320997
YoungJoo Lee <youngker@gmail.com>
2015-08-28 17:45:08 +0900

    add exclude filter.

--- elogcat.el
+++ elogcat.el
@@ -67,8 +67,8 @@
   "DOC."
   :group 'elogcat)
 
-(defvar elogcat-include-filter-regexp "")
-(defvar elogcat-exclude-filter-regexp "")
+(defvar elogcat-include-filter-regexp nil)
+(defvar elogcat-exclude-filter-regexp nil)
 
 (defvar elogcat-enable-klog t)
 
@@ -111,25 +111,69 @@
   (elogcat-stop)
   (elogcat))
 
-(defun elogcat-clear-filter ()
+(defun elogcat-clear-filter (filter)
   "Clear the filter."
   (interactive)
-  (elogcat-set-filter ""))
+  (let ((buffer-read-only nil)
+        msg)
+    (goto-char (point-max))
+    (setq msg (concat "======== " (symbol-name filter) " is cleared ========\n"))
+    (insert
+     (propertize msg 'font-lock-face (cdr (assoc "I" elogcat-face-alist))))
+    (set filter nil)))
 
-(defun elogcat-set-filter (regexp-filter)
-  "Set the filter to REGEXP-FILTER."
-  (interactive "MRegexp Filter: ")
+(defun elogcat-clear-include-filter ()
+  "Clear the include filter."
+  (interactive)
+  (elogcat-clear-filter 'elogcat-include-filter-regexp))
+
+(defun elogcat-clear-exclude-filter ()
+  "Clear the include filter."
+  (interactive)
+  (elogcat-clear-filter 'elogcat-exclude-filter-regexp))
+
+(defun elogcat-set-filter (regexp filter)
+  "Set the filter to REGEXP FILTER."
   (with-current-buffer elogcat-buffer
     (let ((buffer-read-only nil)
           (info-face (cdr (assoc "I" elogcat-face-alist)))
           msg)
       (goto-char (point-max))
-      (if (equal (length regexp-filter) 0)
-          (setq msg "\n\n*** Filter is cleared ***\n\n")
-        (setq msg (concat "\n\n*** Filter is changed to '" regexp-filter
-                          "' ***\n\n")))
-      (insert (propertize msg 'font-lock-face info-face))))
-  (setq elogcat-include-filter-regexp regexp-filter))
+      (setq msg (concat "======== " (symbol-name filter)
+                        " '" regexp "' ========\n"))
+      (insert (propertize msg 'font-lock-face info-face))
+      (set filter regexp))))
+
+(defun elogcat-show-status ()
+  (interactive)
+  (let ((buffer-read-only nil)
+        msg)
+    (goto-char (point-max))
+    (setq msg (concat (symbol-name 'elogcat-include-filter-regexp)
+                      ": '" elogcat-include-filter-regexp "' ,"
+                      (symbol-name 'elogcat-exclude-filter-regexp)
+                      ": '" elogcat-exclude-filter-regexp "'\n"))
+    (insert (propertize msg 'font-lock-face (cdr (assoc "I" elogcat-face-alist))))))
+
+(defun elogcat-set-include-filter (regexp)
+  "set the include filter."
+  (interactive "MRegexp Include Filter: ")
+  (elogcat-set-filter regexp 'elogcat-include-filter-regexp))
+
+(defun elogcat-set-exclude-filter (regexp)
+  "set the exclude filter."
+  (interactive "MRegexp Exclude Filter: ")
+  (elogcat-set-filter regexp 'elogcat-exclude-filter-regexp))
+
+(defun elogcat-include-string-p (line)
+  (if elogcat-include-filter-regexp
+      (s-match elogcat-include-filter-regexp line)
+    t))
+
+(defun elogcat-exclude-string-p (line)
+  (if elogcat-exclude-filter-regexp
+      (s-match elogcat-exclude-filter-regexp line)
+    nil))
 
 (defun elogcat-process-filter (process output)
   "Adb PROCESS make line from OUTPUT buffer."
@@ -145,7 +189,8 @@
             (let ((line (substring output pos (match-beginning 0))))
               (setq pos (match-end 0))
               (goto-char (point-max))
-              (when (string-match-p elogcat-include-filter-regexp line)
+              (when (and (elogcat-include-string-p line)
+                         (not (elogcat-exclude-string-p line)))
                 (if (string-match-p "^\\([0-9][0-9]\\)-\\([0-9][0-9]\\)" line)
                     (let* ((log-list (s-split-up-to "\s+" line 6))
                            (level (nth 4 log-list))
@@ -196,9 +241,12 @@
   (setq elogcat-mode-map (make-sparse-keymap)))
 
 (--each '(("C" . elogcat-erase-buffer)
-          ("f" . elogcat-set-filter)
+          ("i" . elogcat-set-include-filter)
+          ("x" . elogcat-set-exclude-filter)
+          ("I" . elogcat-clear-include-filter)
+          ("X" . elogcat-clear-exclude-filter)
+          ("g" . elogcat-show-status)
           ("F" . occur)
-          ("c" . elogcat-clear-filter)
           ("q" . elogcat-delete-window)
           ("m" . elogcat-toggle-main)
           ("s" . elogcat-toggle-system)
@@ -224,7 +272,8 @@
     (let ((proc (start-process-shell-command
                  "elogcat"
                  elogcat-buffer
-                 (concat "adb shell " (shell-quote-argument
+                 (concat "adb shell "
+                         (shell-quote-argument
                                        (concat elogcat-logcat-command
                                                (when elogcat-enable-klog
                                                  (concat
